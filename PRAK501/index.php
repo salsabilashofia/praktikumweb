2704353bc73285cd1ec7a498c2d8e51aa912bc87
salsabilashofia <101611648+salsabilashofia@users.noreply.github.com>
2022-05-15 19:26:24 +0800

    Create index.php

---
+++ PRAK501/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Perpustakaan</title>
+</head>
+<body>
+    <table style = "">
+        <tr>
+            <th><a href="Buku.php">Data Buku<br></a></th>
+            <th><a href="Member.php">Data Member<br/></a></th>
+            <th><a href="Peminjaman.php">Data Peminjaman<br/></a></th>
+        </tr>
+    </table>
+</body>
+</html>
